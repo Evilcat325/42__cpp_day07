2d806cd3cb12eaead41df1478cfb9bd4f5136449
Sen Li <ls.learnshare@gmail.com>
2019-05-09 02:45:56 -0700

    ex01

--- ex01/iter.cpp
+++ ex01/iter.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 
-template <typename T>
-void print(T const &input)
+template <typename U>
+void print(U const &input)
 {
 	std::cout << input << std::endl;
 }
-
-// template <typename T, template <typename> typename F>
-// void iter(T *array, unsigned long n, F<T> op)
-// {
-// 	for (int i = 0; i < n; i++)
-// 		op(array[i]);
-// }
 
 template <typename T, typename F>
 void iter(T *array, unsigned long n, F op)
@@ -20,12 +13,56 @@ void iter(T *array, unsigned long n, F op)
 		op(array[i]);
 }
 
+// well guess we don't have fully functional programing until c++11 with lambda
+// cannot pass a template template function as a argument...
+// template <typename T, template <typename> typename F>
+// void iter(T *array, unsigned long n, F<T> op)
+// {
+// 	for (int i = 0; i < n; i++)
+// 		op(array[i]);
+// }
+
+// well I guess this way compliler knows where to call the template function
+// we could elimiate the need to indicate type twice....
+// guess too much overheal lol....
+template <typename T>
+class print_fn
+{
+public:
+	T input;
+	print_fn(T input) : input(input){};
+	void operator()()
+	{
+		std::cout << input << std::endl;
+	}
+};
+
+template <typename T, template <typename> class F>
+class iter_fn
+{
+public:
+	T *array;
+	unsigned long n;
+	iter_fn(T *array, unsigned long n) : array(array), n(n){};
+	void operator()()
+	{
+		for (unsigned long i = 0; i < n; i++)
+		{
+			F<T> pfn(array[i]);
+			pfn();
+		}
+	}
+};
+
 int main()
 {
 	int ints[] = {4, 5, 6, 7, 8};
 	iter<int>(ints, sizeof(ints) / sizeof(int), print<int>);
-	//float floats[] = {4.2, 5.2, 6.2, 7.2, 8.2};
-	// iter(floats, sizeof(floats) / sizeof(float), &print);
-	// std::string strings[] = {"abc", "this", "that"};
-	// iter(strings, sizeof(strings) / sizeof(std::string), &print);
+	float floats[] = {4.2, 5.2, 6.2, 7.2, 8.2};
+	iter(floats, sizeof(floats) / sizeof(float), print<float>);
+	std::string strings[] = {"abc", "this", "that"};
+	iter(strings, sizeof(strings) / sizeof(std::string), print<std::string>);
+	(iter_fn<int, print_fn>(ints, sizeof(ints) / sizeof(int)))();
+	(iter_fn<float, print_fn>(floats, sizeof(floats) / sizeof(float)))();
+	(iter_fn<std::string, print_fn>(strings, sizeof(strings) / sizeof(std::string)))();
 }
